f810c8f1c180aee0a1c354ab60140ed526ad08d6
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-29 13:43:03 -0700

    Introduce S3 Store replacing File Store.

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,11 +1,15 @@
 package org.superbiz.moviefun;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.superbiz.moviefun.blobstore.BlobStore;
-import org.superbiz.moviefun.blobstore.FileStore;
+import org.superbiz.moviefun.blobstore.S3Store;
 
 @SpringBootApplication
 public class Application {
@@ -19,8 +23,15 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
+    @Value("${s3.accessKey}") String s3AccessKey;
+    @Value("${s3.secretKey}") String s3SecretKey;
+    @Value("${s3.bucketName}") String s3BucketName;
+
     @Bean
     public BlobStore blobStore() {
-        return new FileStore();
+        AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+
+        return new S3Store(s3Client, s3BucketName);
     }
 }
