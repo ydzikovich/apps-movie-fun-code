60c91c5bc984b03266db3aaaf1fe6b376232a714
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-29 11:26:20 -0700

    Extracted BlobStore interface and FileStore implementation.

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.superbiz.moviefun.blobstore.BlobStore;
+import org.superbiz.moviefun.blobstore.FileStore;
 
 @SpringBootApplication
 public class Application {
@@ -15,5 +17,10 @@ public class Application {
     @Bean
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+
+    @Bean
+    public BlobStore blobStore() {
+        return new FileStore();
     }
 }
