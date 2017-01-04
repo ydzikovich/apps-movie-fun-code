e5c45797fd175814599b2da43fb6560e8df40667
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-04 14:53:29 -0700

    Import Albums from BlobStore at regular interval.

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -23,6 +23,7 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
+    @Value("${s3.endpointUrl}") String s3EndpointUrl;
     @Value("${s3.accessKey}") String s3AccessKey;
     @Value("${s3.secretKey}") String s3SecretKey;
     @Value("${s3.bucketName}") String s3BucketName;
@@ -31,6 +32,7 @@ public class Application {
     public BlobStore blobStore() {
         AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
         AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        s3Client.setEndpoint(s3EndpointUrl);
 
         return new S3Store(s3Client, s3BucketName);
     }
