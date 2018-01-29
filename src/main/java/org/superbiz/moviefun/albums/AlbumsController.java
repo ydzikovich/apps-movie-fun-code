c8cd555e8529163cc8f2eda202164dd719400467
Yuliya Dzikovich <ydzikovich@pivotal.io>
2018-01-29 14:55:01 -0700

    added logging

--- src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -1,6 +1,8 @@
 package org.superbiz.moviefun.albums;
 
 import org.apache.tika.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,6 +27,7 @@ public class AlbumsController {
 
     private final AlbumsBean albumsBean;
     private final BlobStore blobStore;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public AlbumsController(AlbumsBean albumsBean, BlobStore blobStore) {
         this.albumsBean = albumsBean;
@@ -46,14 +49,14 @@ public class AlbumsController {
 
     @PostMapping("/{albumId}/cover")
     public String uploadCover(@PathVariable Long albumId, @RequestParam("file") MultipartFile uploadedFile) {
-        System.out.println("Uploading cover for album with id " + albumId);
+        logger.debug("Uploading cover for album with id {}", albumId);
 
         if (uploadedFile.getSize() > 0) {
             try {
                 tryToUploadCover(albumId, uploadedFile);
 
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("There was an error while uploading album cover", e);
             }
         }
 
--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -16,6 +16,8 @@
  */
 package org.superbiz.moviefun.movies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,14 +34,17 @@ public class MoviesBean {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public Movie find(Long id) {
         return entityManager.find(Movie.class, id);
     }
 
     @Transactional
     public void addMovie(Movie movie) {
-        System.err.println("Creating movie with title " + movie.getTitle() +
-            ", and year " + movie.getYear());
+        logger.debug("Creating movie with title {}, and year {}", movie.getTitle(), movie.getYear());
+
+        logger.trace("This is a trace log!");
 
         entityManager.persist(movie);
     }
